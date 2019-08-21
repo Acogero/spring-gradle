64a0ce58574bdef2e69b0e7dda7655c9a3690599
Marcos Vinicius <techpark.marcos.moreira@gmail.com>
2019-08-21 15:12:10 -0300

    Inserindo projeto

---
+++ src/main/java/com/tipiniquim/apig/ApigApplication.java
@@ -0,0 +1,13 @@
+package com.tipiniquim.apig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigApplication.class, args);
+	}
+
+}
