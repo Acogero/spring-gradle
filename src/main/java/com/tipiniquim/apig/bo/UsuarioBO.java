e98a6519699be93955f6d4d6e03443e2f3979d0b
Marcos Vinicius <techpark.marcos.moreira@gmail.com>
2019-08-26 13:57:30 -0300

    UsuarioBO

---
+++ src/main/java/com/tipiniquim/apig/bo/UsuarioBO.java
@@ -0,0 +1,35 @@
+package com.tipiniquim.apig.bo;
+
+import com.tipiniquim.apig.Util.ServerUtil;
+import com.tipiniquim.apig.Util.UsuarioResponse;
+import com.tipiniquim.apig.dao.UsuarioDAO;
+import com.tipiniquim.apig.modelo.Usuario;
+
+import javax.validation.ConstraintViolationException;
+
+public class UsuarioBO {
+
+    public String inserir(Usuario u, UsuarioDAO dao) {
+        Usuario p2 = dao.existeUsuario(u.getEmail());
+
+        if (p2 != null) {
+            if (u.equals(p2))
+                return UsuarioResponse.USER_FOUND;
+        } else {
+            try {
+                dao.save(u);
+            } catch(ConstraintViolationException e) {
+                ServerUtil.retornoViolacao(e);
+            }
+
+            p2 = dao.existeUsuario(u.getEmail());
+
+            if (p2.getEmail().equals(u.getEmail()))
+                return UsuarioResponse.USER_CREATED;
+            else
+                return UsuarioResponse.ERRO_INVALIDO;
+        }
+
+        return UsuarioResponse.ERRO_INVALIDO;
+    }
+}
